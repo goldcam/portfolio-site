3259e523b550b0d625c8275bd364912ed4e0eb45
cameron.townsend <13910756+goldcam@users.noreply.github.com>
2025-01-23 21:44:30 -0800

    padding for header mobile.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ import Nav from '../nav';
 export class HeaderComponent extends PureComponent {
     render () {
         return (
-            <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-3/12 lg:flex-col lg:justify-between lg:py-20">
+            <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-3/12 lg:flex-col lg:justify-between pt-4 lg:py-20">
             <div>
                     <Name />
                     <Nav />
